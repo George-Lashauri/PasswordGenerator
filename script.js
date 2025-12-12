0323a7355e2dac63aef35c15b0406e9a23461d69
Georga-Lashauri <georgelashauri@gmail.com>
2025-12-12 23:58:37 +0400

    passw gen done

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "{", "]", "}", "\\", "|", ";", ":", "'", "\"", ",", "<", ".", ">", "?", "/"];
 
+const characters = [
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", 
+    "+", "=", "[", "{", "]", "}", "\\", "|", ";", ":", "'", "\"", ",", 
+    "<", ".", ">", "?", "/"
+];
+
+
+const passwordLength = 20;
+
+function generatePassword() {
+    
+    let password = "";
+
+    for (let i = 0; i < passwordLength; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length);      
+        let randomCharacter = characters[randomIndex];
+        password = password + randomCharacter;
+    }
+    let passwordElement = document.getElementById("password-el");
+    passwordElement.textContent = password;
+    
+}
